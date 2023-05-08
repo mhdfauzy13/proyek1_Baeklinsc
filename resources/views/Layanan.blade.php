feb99b11a50582783e762fb174adbf3d942d6dc6
Mhd Fauzy <moha.fauzy09@gmail.com>
2023-05-08 17:37:08 +0700

    nambahin layanan sama asset

---
+++ resources/views/Layanan.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Layanan</title>
+    @vite('../resources/css/app.css')
+    
+</head>
+<body>
+    <div>
+        <aside class="w-[230px]">
+            <div class="flex pb-[50px]">
+               <img src="/assets/image/logoBaeklin.png" class="pt-4" width="100px"/>
+               <img src="/assets/image/Baeklin Shoes Cleaning.png"class="pt-4" width="100px"/>
+            </div>
+            <div class='text-lg px-2 border-y-2 border-black py-10 '>  
+                <p class="pl-14"><b>Dashboard</b></p>
+                <p class="pl-14"><b>Layanan</b></p>
+                <p class="pl-14"><b>Transaksi</b></p>
+                <p class="pl-14"><b>History</b></p>
+            </div>
+        </aside>
+    </div>
+</body>
+</html>   
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/layanan', function () {
+    return view('Layanan');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
